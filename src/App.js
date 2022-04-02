a5c3220cfae0099d21ad60eb45a92e8fa32a243c
Akshat Shukla <akshatshukla175@gmail.com>
2022-04-02 23:19:58 +0530

    rendering news articles

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
+import NewsCards from './components/NewsCards'
+import { Routes } from 'react-router-dom';
 const API_KEY = process.env.REACT_APP_KEY;
 
 function News() {
-  const [value, setValue] = useState("");
-  let newscards;
-  async function getNews() {
-    const response = await fetch("https://newsapi.org/v2/everything?q=Apple&from=2022-04-02&sortBy=popularity&apiKey=3883022e9b294a0f9c42dfd8edfb6cd6");
-    const result = await response.json();
-    newscards = result.articles.map((a) => { return (
-      <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" src={a.urlToImage} alt="" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{a.title}</div>
-        <p class="text-gray-700 text-base">
-          {a.description}
-        </p>
-        <a class="font-bold-text-xl" href={a.url}>Read More</a>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2022-04-02&' +
+          'sortBy=popularity&' +
+          'apiKey=a57ce69f66c642589f4848126f16cb2a';
+    var req = new Request(url);
+    console.log(req);
+    const getNews = async() => {
+      try {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getNews();
+  },[]);
+ 
+  
+  console.log(data);
+  return (
+    <div className="ui container">
+        <Router>
+          <Routes>
+            <Route
+                  path="/"
+                  exact
+                  element={(
+                      <NewsCards
+                          data={data}
+                      />
+                  )}
+              />
+          </Routes>   
+        </Router>
       </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-      </div>
-    </div>
-  </div>
-  )
-   
- });
-  setValue(newscards);
-}
-  getNews();
-  console.log(newscards);
-  return <div>{value}</div>
+    );
+    
 }
 
 export default News;
---
+++ src/components/NewsCards.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const NewsCards = (props) => {
+    console.log(props.data);
+    if (props.data != null) {
+        const newscards = props.data.articles.map((card) => 
+    { 
+        return (   
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src={card.urlToImage} alt="" />
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{card.title}</div>
+            <p class="text-gray-700 text-base">
+            {card.description}
+            </p>
+            <a class="font-bold-text-xl" href={card.url}>Read More</a>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+        </div>
+    )});
+    return (
+        <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">    
+        {newscards}
+        </div>
+    );    
+    }
+    
+    
+}
+
+export default NewsCards;
