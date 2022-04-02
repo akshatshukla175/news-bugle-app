269e62ac2264e947dedcac3f5959570f94b424bf
Akshat Shukla <akshatshukla175@gmail.com>
2022-04-02 11:00:48 +0530

    creating react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function News() {
+  const [value, setValue] = useState("");
+  getNews()
+}
+
+async function getNews() {
+  const response = await fetch("REQUEST");
+  const request = await response.json;
+  console.log(request);
+  console.log(request.articles)
+}
+
+export default News;
